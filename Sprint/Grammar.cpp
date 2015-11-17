47d60b90e45990eacf868d1d484af45da6871f8f
KurtGodel <mtredding@yahoo.com>
2015-11-17 11:57:52 -0600

    organized grammar rules

--- Sprint/Grammar.cpp
+++ Sprint/Grammar.cpp
@@ -18,11 +18,15 @@
 
 Grammar::Grammar() {
 	rules.push_back(GrammarRule(GrammarObject(GENERAL),				{Token(UNKNOWN, "")},													{})); // "null" rule
+	
+	// enum
 	rules.push_back(GrammarRule(GrammarObject(GENERAL),				{Token(KEYWORD, "enum")},												{GrammarObject(ENUM), GrammarObject(GENERAL)}));
 	rules.push_back(GrammarRule(GrammarObject(ENUM),				{Token(KEYWORD, "enum"), Token(NEWLINE, "\n"), Token(INDENT)},			{GrammarObject(TERMINAL, KEYWORD, "enum"), GrammarObject(TERMINAL, NEWLINE, "\n"), GrammarObject(TERMINAL, INDENT), GrammarObject(ENUM_DECLARATION), GrammarObject(ENUM_DECLARATIONS), GrammarObject(TERMINAL, DEDENT)}));
 	rules.push_back(GrammarRule(GrammarObject(ENUM_DECLARATIONS),	{Token(PUNCTUATION, ",")},												{GrammarObject(TERMINAL, PUNCTUATION, ","), GrammarObject(ENUM_DECLARATION), GrammarObject(ENUM_DECLARATIONS)}));
 	rules.push_back(GrammarRule(GrammarObject(ENUM_DECLARATION),	{Token(IDENTIFIER), Token(PUNCTUATION, "="), Token(INTEGER_LITERAL)},	{GrammarObject(TERMINAL, IDENTIFIER), GrammarObject(TERMINAL, PUNCTUATION, "="), GrammarObject(TERMINAL, INTEGER_LITERAL)}));
 	rules.push_back(GrammarRule(GrammarObject(ENUM_DECLARATION),	{Token(IDENTIFIER)},													{GrammarObject(TERMINAL, IDENTIFIER)}));
+	
+	// class
 	rules.push_back(GrammarRule(GrammarObject(GENERAL),				{Token(KEYWORD, "class")},												{GrammarObject(CLASS), GrammarObject(GENERAL)}));
 	rules.push_back(GrammarRule(GrammarObject(CLASS),				{Token(KEYWORD, "class")},												{GrammarObject(TERMINAL, KEYWORD, "class"), GrammarObject(TERMINAL, IDENTIFIER), GrammarObject(TERMINAL, NEWLINE, "\n"), GrammarObject(TERMINAL, INDENT), GrammarObject(DECLARATIONS), GrammarObject(TERMINAL, DEDENT)}));
 	rules.push_back(GrammarRule(GrammarObject(DECLARATIONS),		{Token(KEYWORD, "int"), Token(IDENTIFIER), Token(BRACKET, "(")},		{GrammarObject(METHOD_DECLARATION), GrammarObject(TERMINAL, NEWLINE, "\n"), GrammarObject(DECLARATIONS)}));
