7dc992e11cf46c9c8b0e1424eea26b137666db35
Thomas-Redding <mtredding@yahoo.com>
2016-08-21 17:35:47 -0500

    merged things

--- src/MorganRules.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/ThomasParser.hpp"
-
-
-void addMorganRules(std::vector<ThomasParseRule> &listOfRules) {
-	// listOfRules.push_back(ThomasParseRule(30, general, {parenthesis_block, T_PLUS, parenthesis_block}, add_clause));
-
-	// listOfRules.push_back(ThomasParseRule())
-}
